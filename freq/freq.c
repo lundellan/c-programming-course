0ed6731681b6e86c08b259baf95fe651718ceafb
Jacob Lundell <jacoblundell@hotmail.se>
2024-04-03 23:03:23 +0200

    Fixed indentation

--- freq/freq.c
+++ freq/freq.c
@@ -3,77 +3,79 @@
 #include <string.h>
 
 typedef struct word_t {
-    char* word;
-    int freq;
+  char* word;
+  int freq;
 } word_t;
 
 int isPrime(int num) {
-    int i;
-    if (num == 1)
-        return 0;
+  int i;
+  if (num == 1)
+    return 0;
 
-    for (i = 2; i <= num / 2; i++) {
-        if (num % i == 0)
-            return 0;
-    }
+  for (i = 2; i <= num / 2; i++) {
+    if (num % i == 0)
+      return 0;
+  }
 
-    return 1;
+  return 1;
 }
 
 int main() {
-    char* line = calloc(1048, sizeof(char));
+  char* line = calloc(1048, sizeof(char));
 
-    unsigned int used = 0, i, found = 0, line_nbr = 1;
-    word_t* words = calloc(10000, sizeof(word_t));
+  unsigned int used = 0, i, found = 0, line_nbr = 1;
+  word_t* words = calloc(10000, sizeof(word_t));
 
-    for (i = 0; i < 10000; i++) {
-      words[i].word = calloc(1048, sizeof(char));
-    }
+  for (i = 0; i < 10000; i++) {
+    words[i].word = calloc(1048, sizeof(char));
+  }
 
-    while (fgets(line, 1048, stdin) != NULL) {
-        line[strlen(line)-1] = '\0';
+  while (fgets(line, 1048, stdin) != NULL) {
+    line[strlen(line) - 1] = '\0';
 
-        for (i = 0; i < used; i++) {
-            if (!strcmp(words[i].word, line)) {
-                if (words[i].freq == 0) {
-                    break;
-                }
-                found = 1;
-                break;
-            }
+    for (i = 0; i < used; i++) {
+      if (!strcmp(words[i].word, line)) {
+        if (words[i].freq == 0) {
+          break;
         }
-        if (!found) {
-            if (!isPrime(line_nbr)) {
-                strcpy(words[i].word, line);
-                words[i].freq = 1;
-                used++;
-                printf("added %s\n", line);
-            } else
-                printf("trying to delete %s: not found\n", line);
-
-        } else if (isPrime(line_nbr)) {
-            words[i].freq = 0;
-            printf("trying to delete %s: deleted\n", line);
-        } else {
-            printf("counted %s\n", line);
-            words[i].freq++;
-        }
-        found = 0;
-        line_nbr++;
+        found = 1;
+        break;
+      }
     }
-
-    word_t best = words[0];
-    for (i = 1; i < used; i++) {
-        if (words[i].freq > best.freq)
-            best = words[i];
+    if (!found) {
+      if (!isPrime(line_nbr)) {
+        strcpy(words[i].word, line);
+        words[i].freq = 1;
+        used++;
+        printf("added %s\n", line);
+      }
+      else
+        printf("trying to delete %s: not found\n", line);
     }
+    else if (isPrime(line_nbr)) {
+      words[i].freq = 0;
+      printf("trying to delete %s: deleted\n", line);
+    }
+    else {
+      printf("counted %s\n", line);
+      words[i].freq++;
+    }
+    found = 0;
+    line_nbr++;
+  }
 
-    printf("result: %s %d\n", best.word, best.freq);
+  word_t best = words[0];
+  for (i = 1; i < used; i++) {
+    if (words[i].freq > best.freq)
+      best = words[i];
+  }
 
-    free(line);
-    for (i = 0; i < 10000; i++)
-        free(words[i].word);
-    free(words);
+  printf("result: %s %d\n", best.word, best.freq);
 
-    return 0;
+  free(line);
+  for (i = 0; i < 10000; i++)
+    free(words[i].word);
+  free(words);
+
+  return 0;
 }
--- rpn/rpn.c
+++ rpn/rpn.c
@@ -1,7 +1,7 @@
-#include <stdio.h>
 #include <ctype.h>
+#include <stdio.h>
 
-#define N		(10)
+#define N (10)
 
 #define isFull() (current_index == N - 1)
 #define isEmpty() (current_index == -1)
@@ -16,111 +16,116 @@ int error_found = 0;
 int finished = 0;
 int current_line = 1;
 
-void error()	{
-	error_found = 1;
-	current_index = -1;
-	current_line++;
+void error() {
+  error_found = 1;
+  current_index = -1;
+  current_line++;
 }
 
-int main(void)	{
-	int current_char = 0, parsed_int = 0, print_line = 1;
+int main(void) {
+  int current_char = 0, parsed_int = 0, print_line = 1;
 
-	int term_1, term_2;
+  int term_1, term_2;
 
-	while (!finished)	{
-		if (error_found)	{
-			while (current_char != '\n')	{
-				current_char = getchar();
-			}
-			error_found = 0;
-		}
+  while (!finished) {
+    if (error_found) {
+      while (current_char != '\n') {
+        current_char = getchar();
+      }
+      error_found = 0;
+    }
 
-		print_line = 1;
+    print_line = 1;
 
-		while (!error_found)	{
-			current_char = getchar();
-			if (print_line)	{
-				if (current_char != EOF)	{
-					printf("line %d: ", current_line);
-					print_line = 0;
-				} else	{
-					finished = 1;
-					break;
-				}
-			}
+    while (!error_found) {
+      current_char = getchar();
+      if (print_line) {
+        if (current_char != EOF) {
+          printf("line %d: ", current_line);
+          print_line = 0;
+        }
+        else {
+          finished = 1;
+          break;
+        }
+      }
 
-			if (isdigit(current_char))	{
-				if (isFull())	{
-					printf("error at %c\n", current_char);
-					error();
-					break;
-				}
+      if (isdigit(current_char)) {
+        if (isFull()) {
+          printf("error at %c\n", current_char);
+          error();
+          break;
+        }
 
-				parsed_int = current_char - '0';
-				current_char = getchar();
+        parsed_int = current_char - '0';
+        current_char = getchar();
 
-				while (isdigit(current_char))	{
-					parsed_int = parsed_int * 10 + (current_char - '0');
+        while (isdigit(current_char)) {
+          parsed_int = parsed_int * 10 + (current_char - '0');
 
-					current_char = getchar();
-				}
+          current_char = getchar();
+        }
 
-				if (!push(parsed_int))	{
-					printf("error at %d\n", parsed_int);
-					error();
-					break;
-				}
-			}
+        if (!push(parsed_int)) {
+          printf("error at %d\n", parsed_int);
+          error();
+          break;
+        }
+      }
 
-			if (current_char != ' ')	{
-				if (current_char == '\n')	{
-					if (!pop(&term_1) || !isEmpty())	{
-						printf("error at \\n\n");
-						error();
-						break;
-					} else	{
-						printf("%d\n", term_1);
-					}
+      if (current_char != ' ') {
+        if (current_char == '\n') {
+          if (!pop(&term_1) || !isEmpty()) {
+            printf("error at \\n\n");
+            error();
+            break;
+          }
+          else {
+            printf("%d\n", term_1);
+          }
 
-					current_line++;
-					break;
-				} else if (current_char == EOF)	{
-					finished = 1;
-					break;
-				} else	{
-					if (!pop(&term_2) || !pop(&term_1))	{
-						printf("error at %c\n", current_char);
-						error();
-						break;
-					}
+          current_line++;
+          break;
+        }
+        else if (current_char == EOF) {
+          finished = 1;
+          break;
+        }
+        else {
+          if (!pop(&term_2) || !pop(&term_1)) {
+            printf("error at %c\n", current_char);
+            error();
+            break;
+          }
 
-					switch (current_char)	{
-						case '+':
-							push(term_1 + term_2);
-							break;
-						case '-':
-							push(term_1 - term_2);
-							break;
-						case '*':
-							push(term_1 * term_2);
-							break;
-						case '/':
-							if(term_2 == 0){
-								printf("error at /\n");
-								error();
-							}else{
-								push(term_1 / term_2);
-							}
-							break;
-						default:
-							printf("error at %c\n", current_char);
-							error();
-							break;
-					}
-				}
-			}
-		}
-	}
+          switch (current_char) {
+            case '+':
+              push(term_1 + term_2);
+              break;
+            case '-':
+              push(term_1 - term_2);
+              break;
+            case '*':
+              push(term_1 * term_2);
+              break;
+            case '/':
+              if (term_2 == 0) {
+                printf("error at /\n");
+                error();
+              }
+              else {
+                push(term_1 / term_2);
+              }
+              break;
+            default:
+              printf("error at %c\n", current_char);
+              error();
+              break;
+          }
+        }
+      }
+    }
+  }
 
-	return 0;
+  return 0;
 }
